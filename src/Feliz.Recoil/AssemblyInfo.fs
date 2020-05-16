026ce4d400755b420f6029fb428189b31d4ba4d1
Shmew <Shmeww@gmail.com>
2020-05-16 01:03:15 -0500

    Publishing docs

--- src/Feliz.Recoil/AssemblyInfo.fs
+++ src/Feliz.Recoil/AssemblyInfo.fs
@@ -5,18 +5,18 @@ open System.Runtime.CompilerServices
 
 [<assembly: AssemblyTitleAttribute("Feliz.Recoil")>]
 [<assembly: AssemblyProductAttribute("Feliz.Recoil")>]
-[<assembly: AssemblyDescriptionAttribute("Fable bindings written in the Feliz-style for Facebook's recoil.")>]
+[<assembly: AssemblyDescriptionAttribute("Fable bindings in Feliz style for Facebook's experimental state management library recoil.")>]
 [<assembly: AssemblyVersionAttribute("0.0.1")>]
 [<assembly: AssemblyFileVersionAttribute("0.0.1")>]
-[<assembly: AssemblyConfigurationAttribute("Debug")>]
+[<assembly: AssemblyConfigurationAttribute("Release")>]
 [<assembly: InternalsVisibleToAttribute("Feliz.Recoil.Tests")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Feliz.Recoil"
     let [<Literal>] AssemblyProduct = "Feliz.Recoil"
-    let [<Literal>] AssemblyDescription = "Fable bindings written in the Feliz-style for Facebook's recoil."
+    let [<Literal>] AssemblyDescription = "Fable bindings in Feliz style for Facebook's experimental state management library recoil."
     let [<Literal>] AssemblyVersion = "0.0.1"
     let [<Literal>] AssemblyFileVersion = "0.0.1"
-    let [<Literal>] AssemblyConfiguration = "Debug"
+    let [<Literal>] AssemblyConfiguration = "Release"
     let [<Literal>] InternalsVisibleTo = "Feliz.Recoil.Tests"
